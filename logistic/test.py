2985d9142a0e4a4405d5a3b0fa2653be354ed087
Андрющенко Вячеслав <andryushchenko@sd.local>
2023-03-31 16:33:29 +0700

    workflows_6

--- logistic/test.py
+++ logistic/test.py
@@ -5,5 +5,5 @@ from rest_framework.test import APIClient
 class TestS(TestCase):
     def test_samle_view(self):
         client = APIClient()
-        response  = client.get('/api/v1/test/')
+        response = client.get('/api/v1/test/')
         self.assertEqual(response.status_code, 200)
