2465bccd929424bbcdbdfad00c20d0d27fb8d67e
Андрющенко Вячеслав <andryushchenko@sd.local>
2023-03-31 12:36:57 +0700

    TestView2

--- logistic/urls.py
+++ logistic/urls.py
@@ -5,6 +5,6 @@ from logistic.views import ProductViewSet, StockViewSet, TestView
 router = DefaultRouter()
 router.register('products', ProductViewSet)
 router.register('stocks', StockViewSet)
-router.register('test', TestView)
+
 
 urlpatterns = router.urls
